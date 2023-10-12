0a3ed43daca52601b9cd787cb569c21260775781
David Hernandez <davidmiguelh@gmail.com>
2023-10-11 22:05:45 -0600

    Connecting to Postgres with Go

---
+++ cmd/exp/psql/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func main() {
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=sa password=@dmin1234 dbname=lenslocked sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected!")
+}
