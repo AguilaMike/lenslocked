7ca631d2c23dfa8547cd0125d484e2fe9ecd728d
David Hernandez <davidmiguelh@gmail.com>
2023-10-15 11:09:24 -0600

    Log Middleware

--- pkg/app/router/router.go
+++ pkg/app/router/router.go
@@ -1,9 +1,14 @@
 package router
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/AguilaMike/lenslocked/pkg/app/controllers"
 	"github.com/AguilaMike/lenslocked/pkg/app/models"
@@ -19,7 +24,7 @@ func Router(r *chi.Mux, userService models.UserService) {
 	// Contact
 	registerGetControllerDefaultFs(r, "/contact", "layout.gohtml", "pages", "contact.gohtml")
 	// FAQ
-	r.Get("/faq", controllers.FAQ(
+	r.Get("/faq", LogMiddleware(controllers.FAQ(
 		views.Must(
 			views.ParseFS(
 				templates.FS,
@@ -27,14 +32,14 @@ func Router(r *chi.Mux, userService models.UserService) {
 				JoinPath("pages", "faq.gohtml"),
 			),
 		),
-	))
+	)))
 
 	usersC := controllers.Users{
 		UserService: &userService,
 	}
 	SignUp(r, usersC)
 	SignIn(r, usersC)
-	r.Get("/users/me", usersC.CurrentUser)
+	r.Get("/users/me", LogMiddleware(usersC.CurrentUser))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("404 Not Found: %s", r.URL.Path), http.StatusNotFound)
@@ -47,8 +52,8 @@ func SignUp(r *chi.Mux, usersC controllers.Users) {
 			templates.FS,
 			JoinPath("layout", "layout.gohtml"),
 			JoinPath("pages", "auth", "signup.gohtml")))
-	r.Get("/signup", usersC.New)
-	r.Post("/signup", usersC.Create)
+	r.Get("/signup", LogMiddleware(usersC.New))
+	r.Post("/signup", LogMiddleware(usersC.Create))
 }
 
 func SignIn(r *chi.Mux, usersC controllers.Users) {
@@ -57,8 +62,8 @@ func SignIn(r *chi.Mux, usersC controllers.Users) {
 			templates.FS,
 			JoinPath("layout", "layout.gohtml"),
 			JoinPath("pages", "auth", "signin.gohtml")))
-	r.Get("/signin", usersC.New)
-	r.Post("/signin", usersC.ProcessSignIn)
+	r.Get("/signin", LogMiddleware(usersC.New))
+	r.Post("/signin", LogMiddleware(usersC.ProcessSignIn))
 }
 
 func registerGetControllerDefaultFs(r *chi.Mux, path, layout string, pages ...string) {
@@ -67,14 +72,58 @@ func registerGetControllerDefaultFs(r *chi.Mux, path, layout string, pages ...st
 
 func registerGetControllerWithTemplateFs(r *chi.Mux, data interface{}, path string, templateFile ...string) {
 	tpl := views.Must(views.ParseFS(templates.FS, templateFile...))
-	r.Get(path, controllers.StaticHandler(tpl, data))
+	r.Get(path, LogMiddleware(controllers.StaticHandler(tpl, data)))
 }
 
 func registerGetControllerWithTemplate(r *chi.Mux, path, templateFolder, templateFile string, data interface{}) {
 	tpl := views.Must(views.Parse(JoinPath(templateFolder, templateFile)))
-	r.Get(path, controllers.StaticHandler(tpl, data))
+	r.Get(path, LogMiddleware(controllers.StaticHandler(tpl, data)))
 }
 
 func JoinPath(path ...string) string {
 	return filepath.Join(path...)
+}
+
+func LogMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		ip, err := getIP(r)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		next.ServeHTTP(w, r)
+		fmt.Printf("Request: IP [%s] Method [%s] Path [%s] Time[%s]\n", ip, r.Method, r.URL.Path, time.Since(start))
+		log.Printf("Request: IP [%s] Method [%s] Path [%s] Time[%s]", ip, r.Method, r.URL.Path, time.Since(start))
+	}
+}
+
+// getIP returns the ip address from the http request
+func getIP(r *http.Request) (string, error) {
+	ips := r.Header.Get("X-Forwarded-For")
+	splitIps := strings.Split(ips, ",")
+
+	if len(splitIps) > 0 {
+		// get last IP in list since ELB prepends other user defined IPs, meaning the last one is the actual client IP.
+		netIP := net.ParseIP(splitIps[len(splitIps)-1])
+		if netIP != nil {
+			return netIP.String(), nil
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		ip := netIP.String()
+		if ip == "::1" {
+			return "127.0.0.1", nil
+		}
+		return ip, nil
+	}
+
+	return "", errors.New("IP not found")
 }
